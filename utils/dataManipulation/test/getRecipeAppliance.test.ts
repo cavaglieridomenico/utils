ce317931a7c5d3bd141a5b91894e04e78b5723f7
cavaglieridomenico <cavaglieridomenico@gmail.com>
2022-12-07 08:34:08 +0100

    added new function with tests

---
+++ utils/dataManipulation/test/getRecipeAppliance.test.ts
@@ -0,0 +1,44 @@
+import { expect, test } from "@jest/globals";
+
+//Find appliance in dataLayer filterChainedDetails
+const getRecipeAppliance = (filterChainedDetails: string) => {
+  const dataLayerfilterChainedDetailsList: string[] =
+    filterChainedDetails?.split("&filters ");
+
+  const targetFilter = dataLayerfilterChainedDetailsList.find((item) =>
+    item.includes("appliances=")
+  );
+  return targetFilter?.replace("filters ", "").replace("appliances=", "");
+};
+
+test("Get the recipe appliance", () => {
+  expect(getRecipeAppliance("filters appliances=microonde")).toBe("microonde");
+  expect(
+    getRecipeAppliance(
+      "filters appliances=microonde&filters dishes=primi e secondi&filters dishes=dolci"
+    )
+  ).toBe("microonde");
+  expect(
+    getRecipeAppliance(
+      "filters dishes=primi e secondi&filters dishes=dolci&filters appliances=forno"
+    )
+  ).toBe("forno");
+  expect(getRecipeAppliance("filters appliances=slower juiser")).toBe(
+    "slower juiser"
+  );
+  expect(
+    getRecipeAppliance(
+      "filters appliances=slower juiser&filters dishes=estratti"
+    )
+  ).toBe("slower juiser");
+  expect(
+    getRecipeAppliance(
+      "filters dishes=estratti&filters appliances=slower juiser"
+    )
+  ).toBe("slower juiser");
+  expect(
+    getRecipeAppliance(
+      "filters dishes=primi e secondi&filters dishes=antipasti&filters appliances=forno"
+    )
+  ).toBe("forno");
+});
