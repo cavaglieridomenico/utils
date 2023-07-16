81956f1ab42c0224c27736a04fb2a07479e22484
cavaglieridomenico <cavaglieridomenico@gmail.com>
2023-07-16 16:36:34 +0200

    Add new test

--- utils/various/various.test.ts
+++ utils/various/various.test.ts
@@ -8,3 +8,22 @@ test("Array without duplicates", () => {
     "dog",
   ]);
 });
+
+const getNewList = (list) =>
+  list.map((item) => ({
+    newPet: item.pet,
+    location: "Home",
+  }));
+
+const list = [{ pet: "Cat" }, { pet: "Dog" }, { pet: "Rabbit" }];
+
+test("New list of Objects", () => {
+  expect(getNewList(list)).toStrictEqual([
+    { newPet: "Cat", location: "Home" },
+    { newPet: "Dog", location: "Home" },
+    { newPet: "Rabbit", location: "Home" },
+    // "Cats",
+    // "Dogs",
+    // "Rabbits",
+  ]);
+});
