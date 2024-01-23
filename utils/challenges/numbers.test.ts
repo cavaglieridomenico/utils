e07c2f359bdf1c44a928769a598d9e5d50ec050e
cavaglieridomenico@gmail.com <cavaglieridomenico@gmail.com>
2024-01-23 20:34:14 +0100

    Add various updates

--- utils/challenges/numbers.test.ts
+++ utils/challenges/numbers.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "@jest/globals";
 
 /*Return true if 100*/
-const checkNumbers = (num1: number, num2: number): any => {
+const checkNumbers = (num1: number, num2: number): boolean => {
   return num1 === 100 || num2 === 100 || num1 + num2 === 100;
 };
 
@@ -17,4 +17,45 @@ test("Check numbers", () => {
 test("Check numbers", () => {
   expect(checkNumbers(10, 91)).toStrictEqual(false);
 });
-/*End of Return true if 100*/
+
+/*Return the number nearest to 100*/
+const checkNumbers2 = (num1: number, num2: number): number => {
+  return Math.abs(100 - num1) < Math.abs(100 - num2) ? num1 : num2;
+};
+
+test("Check numbers", () => {
+  expect(checkNumbers2(63, 100)).toStrictEqual(100);
+});
+test("Check numbers", () => {
+  expect(checkNumbers2(60, 15)).toStrictEqual(60);
+});
+test("Check numbers", () => {
+  expect(checkNumbers2(40, 55)).toStrictEqual(55);
+});
+test("Check numbers", () => {
+  expect(checkNumbers2(200, 55)).toStrictEqual(55);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(55, 200)).toStrictEqual(55);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(1000, 20)).toStrictEqual(20);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(101, 98)).toStrictEqual(101);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(1000, 10000)).toStrictEqual(1000);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(10000, 1000)).toStrictEqual(1000);
+});
+
+test("Check numbers", () => {
+  expect(checkNumbers2(50, 50)).toStrictEqual(50);
+});
--- utils/challenges/strings.test.ts
+++ utils/challenges/strings.test.ts
@@ -103,3 +103,89 @@ test("Get new word", () => {
 test("Get new word", () => {
   expect(getNewWord4("extrafresh")).toStrictEqual("extesh");
 });
+
+/*Returns a new string to extract the first half of a string of even length no odd*/
+
+const getNewWord5 = (str: string) =>
+  str.length % 2 !== 0 ? str : str.slice(0, str.length / 2);
+
+test("Get new word", () => {
+  expect(getNewWord5("dogo")).toStrictEqual("do");
+});
+
+test("Get new word", () => {
+  expect(getNewWord5("dog")).toStrictEqual("dog");
+});
+
+/*Returns a new string by concatenating two string except their first character*/
+
+const getNewWord6 = (str1: string, str2: string) =>
+  str1.slice(1) + str2.slice(1);
+
+test("Get new word", () => {
+  expect(getNewWord6("dogo", "mogo")).toStrictEqual("ogoogo");
+});
+
+test("Get new word", () => {
+  expect(getNewWord6("cat", "dog")).toStrictEqual("atog");
+});
+
+/*Check if the occurrence number of a carecter in a string is 2 to 4*/
+
+const getOccurrenceNumber = (
+  word: string,
+  characterToFind: string
+): boolean => {
+  let occurrence = 0;
+  word
+    .split("")
+    .forEach((character) => character === characterToFind && occurrence++);
+  return occurrence >= 2 && occurrence <= 4;
+};
+
+test("Check occurrence number", () => {
+  expect(getOccurrenceNumber("avengers", "e")).toStrictEqual(true);
+});
+
+test("Check occurrence number", () => {
+  expect(getOccurrenceNumber("avengers", "v")).toStrictEqual(false);
+});
+
+test("Check occurrence number", () => {
+  expect(getOccurrenceNumber("cracco", "c")).toStrictEqual(true);
+});
+
+test("Check occurrence number", () => {
+  expect(getOccurrenceNumber("cracco", "z")).toStrictEqual(false);
+});
+
+test("Check occurrence number", () => {
+  expect(getOccurrenceNumber("crcaccoc", "c")).toStrictEqual(false);
+});
+
+const countOccorrences = (word: string, characterToFind: string): number =>
+  word.split("").filter((character) => character === characterToFind).length;
+
+const getOccurrenceNumber2 = (word: string, characterToFind: string): boolean =>
+  countOccorrences(word, characterToFind) >= 2 &&
+  countOccorrences(word, characterToFind) <= 4;
+
+test("Get occurrence number", () => {
+  expect(getOccurrenceNumber2("avengers", "e")).toStrictEqual(true);
+});
+
+test("Get occurrence number", () => {
+  expect(getOccurrenceNumber2("avengers", "v")).toStrictEqual(false);
+});
+
+test("Get occurrence number", () => {
+  expect(getOccurrenceNumber2("cracco", "c")).toStrictEqual(true);
+});
+
+test("Get occurrence number", () => {
+  expect(getOccurrenceNumber2("cracco", "z")).toStrictEqual(false);
+});
+
+test("Get occurrence number", () => {
+  expect(getOccurrenceNumber2("crcaccoc", "c")).toStrictEqual(false);
+});
